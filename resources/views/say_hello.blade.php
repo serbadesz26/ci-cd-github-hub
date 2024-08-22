1ec436beaff38f6544618383ed64c4b38b31a9b1
novriamsyah <novriamsyahh@gmail.com>
2024-08-22 14:37:13 +0700

    feature: add page welcome

---
+++ resources/views/say_hello.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Serbades</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div class="w-dvh h-dvh hero">
+        <div class="flex justify-center items-center flex-col">
+            <h1 class="font-bold text-[3.5em] text-[#F58F84] mb-2">Channel Serbades</h1>
+            <a href="javascript:void(0)" class="" target="_blank">
+                <div class="darksoul-card">
+                    <div class="circle"></div>
+                    <img src="{{ asset('assets/icon/youtube.svg') }}" alt="icon youtube" width="35" height="35" style="z-index: 100;" />
+                    <p class="content">Subscribe</p>
+                </div>
+            </a>
+        </div>
+    </div>
+</body>
+
+</html>
