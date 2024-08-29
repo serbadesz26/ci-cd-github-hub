7d6d83f63a04cde8b48689fff4b391c89a371bae
novriamsyah <novriamsyahh@gmail.com>
2024-08-29 13:51:27 +0700

    feature: ci/cd laravel

--- resources/views/say_hello.blade.php
+++ resources/views/say_hello.blade.php
@@ -5,13 +5,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Serbades</title>
-    @vite('resources/css/app.css')
+
+    <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
 </head>
 
 <body>
-    <div class="w-dvh h-dvh hero">
-        <div class="flex justify-center items-center flex-col">
-            <h1 class="font-bold text-[3.5em] text-[#F58F84] mb-2">Channel Serbades</h1>
+    <div class="hero">
+        <div class="box">
+            <h1 class="font-bold text-title text-secondary mb-2">Channel Serbades</h1>
             <a href="javascript:void(0)">
                 <div class="darksoul-card">
                     <div class="circle"></div>
